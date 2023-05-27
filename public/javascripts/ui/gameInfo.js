28fbdaaeb8ff691792410074c3a87c62e5ba666b
TurboMoscal <124038268+TurboMoscal@users.noreply.github.com>
2023-05-27 11:23:19 +0100

    Update gameInfo.js

--- public/javascripts/ui/gameInfo.js
+++ public/javascripts/ui/gameInfo.js
@@ -44,13 +44,10 @@ class GameInfo  {
         }
         if (GameInfo.game.player.obj.hp <= 0 ) {
             GameInfo.gameoverState = true;
-           GameInfo.sounds.lose.play();
-            
         }
 
         if (GameInfo.game.opponents[0].obj.hp <= 0) {
             GameInfo.gameoverState = true;
-            GameInfo.sounds.win.play();
         }
         GameInfo.endgameButton.hide();
         if (GameInfo.gameoverState == true) {
